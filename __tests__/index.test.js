1f487877d1012d450d309fedce1338cd2cf46602
EnergoStalin <alex.vipers@mail.ru>
2023-07-18 09:58:24 +0300

    fixed tests & ci

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,156 +1,41 @@
 const tag = require('..').templateTags[0];
 
 describe('Response tag', () => {
-  describe('General', () => {
-    it('fails on no responses', async () => {
-      const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
+  it('basic query eval', async () => {
+    const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
 
-      const context = _genTestContext(requests);
-
-      try {
-        await tag.run(context, 'raw', 'req_1', '', 'JSON.parse(output).foo');
-        fail('JSON should have failed');
-      } catch (err) {
-        expect(err.message).toContain('No responses for request');
+    const responses = [
+      {
+        _id: 'res_1',
+        parentId: 'req_1',
+        statusCode: 200,
+        contentType: 'application/json',
+        _body: '{"foo": "bar"}'
       }
-    });
+    ];
 
-    it('fails on no request', async () => {
-      const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
+    const context = _genTestContext(requests, responses);
+    const result = await tag.run(context, 'req_1', '', '', 'JSON.parse(body).foo');
+    expect(result).toBe('bar');
+  })
 
-      const responses = [
-        {
-          _id: 'res_1',
-          parentId: 'req_1',
-          statusCode: 200,
-          _body: '{"foo": "bar"}'
-        }
-      ];
+  it('basic query eval empty', async () => {
+    const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
 
-      const context = _genTestContext(requests, responses);
-
-      try {
-        await tag.run(context, 'raw', 'req_test', '', 'JSON.parse(output).foo');
-        fail('JSON should have failed');
-      } catch (err) {
-        expect(err.message).toContain('Could not find request req_test');
+    const responses = [
+      {
+        _id: 'res_1',
+        parentId: 'req_1',
+        statusCode: 200,
+        contentType: 'application/json',
+        _body: '{"foo": "bar"}'
       }
-    });
-  });
+    ];
 
-  describe('ResponseExtension Header', async () => {
-    it('renders basic response "header"', async () => {
-      const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
-
-      const responses = [
-        {
-          _id: 'res_1',
-          parentId: 'req_1',
-          statusCode: 200,
-          contentType: '',
-          headers: [
-            { name: 'Content-Type', value: 'application/json' },
-            { name: 'Content-Length', value: '20' }
-          ]
-        }
-      ];
-
-      const context = _genTestContext(requests, responses);
-
-      expect(await tag.run(context, 'header', 'req_1', 'content-type')).toBe('application/json');
-      expect(await tag.run(context, 'header', 'req_1', 'Content-Type')).toBe('application/json');
-      expect(await tag.run(context, 'header', 'req_1', 'CONTENT-type')).toBe('application/json');
-      expect(await tag.run(context, 'header', 'req_1', 'CONTENT-type    ')).toBe(
-        'application/json'
-      );
-    });
-
-    it('no results on missing header', async () => {
-      const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
-
-      const responses = [
-        {
-          _id: 'res_1',
-          parentId: 'req_1',
-          statusCode: 200,
-          headers: [
-            { name: 'Content-Type', value: 'application/json' },
-            { name: 'Content-Length', value: '20' }
-          ]
-        }
-      ];
-
-      const context = _genTestContext(requests, responses);
-
-      try {
-        await tag.run(context, 'header', 'req_1', 'missing');
-        fail('should have failed');
-      } catch (err) {
-        expect(err.message).toBe(
-          'No header with name "missing".\n' +
-            'Choices are [\n\t"Content-Type",\n\t"Content-Length"\n]'
-        );
-      }
-    });
-  });
-
-  describe('Raw', async () => {
-    it('renders basic response', async () => {
-      const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
-
-      const responses = [
-        {
-          _id: 'res_1',
-          parentId: 'req_1',
-          statusCode: 200,
-          contentType: 'text/plain',
-          _body: 'Hello World!'
-        }
-      ];
-
-      const context = _genTestContext(requests, responses);
-
-      expect(await tag.run(context, 'raw', 'req_1')).toBe('Hello World!');
-    });
-
-    it('basic query eval', async () => {
-      const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
-
-      const responses = [
-        {
-          _id: 'res_1',
-          parentId: 'req_1',
-          statusCode: 200,
-          contentType: 'application/json',
-          _body: '{"foo": "bar"}'
-        }
-      ];
-
-      const context = _genTestContext(requests, responses);
-      const result = await tag.run(context, 'raw', 'req_1', '', 'JSON.parse(output).foo');
-
-      expect(result).toBe('bar');
-    })
-
-    it('basic query eval empty', async () => {
-      const requests = [{ _id: 'req_1', parentId: 'wrk_1' }];
-
-      const responses = [
-        {
-          _id: 'res_1',
-          parentId: 'req_1',
-          statusCode: 200,
-          contentType: 'application/json',
-          _body: '{"foo": "bar"}'
-        }
-      ];
-
-      const context = _genTestContext(requests, responses);
-      const result = await tag.run(context, 'raw', 'req_1', '', '');
-
-      expect(result).toBe('{"foo": "bar"}');
-    })
-  });
+    const context = _genTestContext(requests, responses);
+    const result = await tag.run(context, 'req_1', '', '', '');
+    expect(result).toBe('{"foo": "bar"}');
+  })
 });
 
 function _genTestContext(requests, responses) {
@@ -162,6 +47,14 @@ function _genTestContext(requests, responses) {
     delete res._body;
   }
   return {
+    context: {
+      getEnvironmentId() {
+        return 0
+      },
+      getExtraInfo() {
+        return []
+      },
+    },
     util: {
       models: {
         request: {
@@ -170,7 +63,7 @@ function _genTestContext(requests, responses) {
           }
         },
         response: {
-          getLatestForRequestId(requestId) {
+          getLatestForRequestId(requestId, _) {
             return responses.find(r => r.parentId === requestId) || null;
           },
           getBodyBuffer(response) {
